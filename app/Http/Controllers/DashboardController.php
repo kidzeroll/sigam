c096c991a7041f528e34481433d2c411af7c09c0
kidzeroll <muhammadhaykall99@gmail.com>
2022-07-31 18:43:26 +0700

    try to add chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,7 +18,7 @@ class DashboardController extends Controller
         $artikel = Artikel::count();
         $chart = $this->chart();
 
-        return view('backend.dashboard.dashboard', compact('penduduk', 'cowo', 'cewe', 'artikel','chart'));
+        return view('backend.dashboard.dashboard', compact('penduduk', 'cowo', 'cewe', 'artikel', 'chart'));
     }
 
     private function chart()
@@ -29,16 +29,17 @@ class DashboardController extends Controller
             ->get();
 
         $chart['SKP'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKBM']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKKM']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKBB']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKU']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKK']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
-        $chart['SKD']=['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKBM'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKKM'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKBB'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKU'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKK'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
+        $chart['SKD'] = ['01' => 0, '02' => 0, '03' => 0, '04' => 0, '05' => 0, '06' => 0, '07' => 0, '08' => 0, '09' => 0, '10' => 0, '11' => 0, '12' => 0];
 
         foreach ($data as $v) {
             $chart[$v->jenis_surat][sprintf('%02d', $v->bulan)] = $v->total;
         }
+
         //sort by month ASC
         $c['SKP'] = [$chart['SKP']['01'], $chart['SKP']['02'], $chart['SKP']['03'], $chart['SKP']['04'], $chart['SKP']['05'], $chart['SKP']['06'], $chart['SKP']['07'], $chart['SKP']['08'], $chart['SKP']['09'], $chart['SKP']['10'], $chart['SKP']['11'], $chart['SKP']['12']];
         $c['SKBM'] = [$chart['SKBM']['01'], $chart['SKBM']['02'], $chart['SKBM']['03'], $chart['SKBM']['04'], $chart['SKBM']['05'], $chart['SKBM']['06'], $chart['SKBM']['07'], $chart['SKBM']['08'], $chart['SKBM']['09'], $chart['SKBM']['10'], $chart['SKBM']['11'], $chart['SKBM']['12']];
@@ -47,7 +48,6 @@ class DashboardController extends Controller
         $c['SKU'] = [$chart['SKU']['01'], $chart['SKU']['02'], $chart['SKU']['03'], $chart['SKU']['04'], $chart['SKU']['05'], $chart['SKU']['06'], $chart['SKU']['07'], $chart['SKU']['08'], $chart['SKU']['09'], $chart['SKU']['10'], $chart['SKU']['11'], $chart['SKU']['12']];
         $c['SKK'] = [$chart['SKK']['01'], $chart['SKK']['02'], $chart['SKK']['03'], $chart['SKK']['04'], $chart['SKK']['05'], $chart['SKK']['06'], $chart['SKK']['07'], $chart['SKK']['08'], $chart['SKK']['09'], $chart['SKK']['10'], $chart['SKK']['11'], $chart['SKK']['12']];
         $c['SKD'] = [$chart['SKD']['01'], $chart['SKD']['02'], $chart['SKD']['03'], $chart['SKD']['04'], $chart['SKD']['05'], $chart['SKD']['06'], $chart['SKD']['07'], $chart['SKD']['08'], $chart['SKD']['09'], $chart['SKD']['10'], $chart['SKD']['11'], $chart['SKD']['12']];
-
 
         return $c;
     }
