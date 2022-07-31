229ce8cf6bee08b3f05f53f18ec67adb26d49d06
kidzeroll <muhammadhaykall99@gmail.com>
2022-07-31 23:02:53 +0700

    first commit

--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -3,7 +3,7 @@
 
     <div class="row ">
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <div class="card">
+            <div class="card card-primary">
                 <div class="card-statistic-4">
                     <div class="align-items-center justify-content-between">
                         <div class="row ">
@@ -15,7 +15,7 @@
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                 <div class="banner-img">
-                                    <img src="{{ asset('backend/assets/img/banner/1.png') }}" alt="">
+                                    <img src="{{ asset('img/penduduk.png') }}" alt="">
                                 </div>
                             </div>
                         </div>
@@ -23,8 +23,9 @@
                 </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <div class="card">
+            <div class="card card-primary">
                 <div class="card-statistic-4">
                     <div class="align-items-center justify-content-between">
                         <div class="row ">
@@ -36,7 +37,7 @@
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                 <div class="banner-img">
-                                    <img src="{{ asset('backend/assets/img/banner/2.png') }}" alt="">
+                                    <img src="{{ asset('img/cowo.png') }}" alt="">
                                 </div>
                             </div>
                         </div>
@@ -44,8 +45,9 @@
                 </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <div class="card">
+            <div class="card card-primary">
                 <div class="card-statistic-4">
                     <div class="align-items-center justify-content-between">
                         <div class="row ">
@@ -57,7 +59,7 @@
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                 <div class="banner-img">
-                                    <img src="{{ asset('backend/assets/img/banner/3.png') }}" alt="">
+                                    <img src="{{ asset('img/cewe.png') }}" alt="">
                                 </div>
                             </div>
                         </div>
@@ -65,8 +67,9 @@
                 </div>
             </div>
         </div>
+
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <div class="card">
+            <div class="card card-primary">
                 <div class="card-statistic-4">
                     <div class="align-items-center justify-content-between">
                         <div class="row ">
@@ -78,7 +81,187 @@
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                 <div class="banner-img">
-                                    <img src="{{ asset('backend/assets/img/banner/4.png') }}" alt="">
+                                    <img src="{{ asset('img/artikel.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row ">
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Kelahiran Bulan ini</h5>
+                                    <h2 class="mb-3 font-18">{{ $kelahiran }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/kalahiran.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Kematian Bulan ini</h5>
+                                    <h2 class="mb-3 font-18">{{ $kematian }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/kematian.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Pendatang Bulan ini</h5>
+                                    <h2 class="mb-3 font-18">{{ $datang }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/pendatang.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Perpindahan Bulan ini</h5>
+                                    <h2 class="mb-3 font-18">{{ $pindah }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/pindah.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row ">
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Surat Masuk</h5>
+                                    <h2 class="mb-3 font-18">{{ $suratMasuk }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/surat-masuk.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Surat Keluar</h5>
+                                    <h2 class="mb-3 font-18">{{ $suratKeluar }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/surat-keluar.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">User</h5>
+                                    <h2 class="mb-3 font-18">{{ $user }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/user.png') }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="card card-primary">
+                <div class="card-statistic-4">
+                    <div class="align-items-center justify-content-between">
+                        <div class="row ">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                                <div class="card-content">
+                                    <h5 class="font-15">Galeri Foto</h5>
+                                    <h2 class="mb-3 font-18">{{ $foto }}</h2>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                                <div class="banner-img">
+                                    <img src="{{ asset('img/galeri.png') }}" alt="">
                                 </div>
                             </div>
                         </div>
@@ -89,18 +272,21 @@
     </div>
 
     <div class="row">
-        <div class="card col-12">
-            <div class="card-header">
-                <h4>Grafik Total Request Administrasi Surat Perbulan</h4>
-            </div>
+        <div class="col-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h4>Grafik Total Request Administrasi Surat Perbulan</h4>
+                </div>
 
-            <div class="card-body">
-                <canvas id="surat-chart"></canvas>
+                <div class="card-body">
+                    <canvas id="surat-chart"></canvas>
+                </div>
             </div>
         </div>
     </div>
 
     @push('script')
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
         <script>
             const ctx = document.getElementById('surat-chart').getContext('2d');
 
