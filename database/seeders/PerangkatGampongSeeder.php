10aea3b7562f04dbb5dc886ec08d825ce21517ca
kidzeroll <muhammadhaykall99@gmail.com>
2022-08-24 20:56:20 +0700

    first commit

--- database/seeders/PerangkatGampongSeeder.php
+++ database/seeders/PerangkatGampongSeeder.php
@@ -10,24 +10,6 @@ class PerangkatGampongSeeder extends Seeder
 {
     public function run()
     {
-        // $faker = \Faker\Factory::create();
-
-        // $jabatan = [
-        //     'keuchik', 'skretaris gampong', 'bendahara gampong', 'operator gampong', 'ketua pemuda', 'ketua keamanan', 'tuha peut', 'kaur umum', 'kaur pembangunan', 'kaur pemerintahan'
-        // ];
-
-        // for ($i = 0; $i < 10; $i++) {
-        //     PerangkatGampong::create([
-        //         'nik' => $faker->numerify('################'),
-        //         'nama' => $faker->name(),
-        //         'jenis_kelamin' => $faker->randomElement(['L', 'P']),
-        //         'jabatan' => $jabatan[$i],
-        //         'no_hp' => $faker->phoneNumber(),
-        //         'alamat' => $faker->address(),
-        //         'photo_path' => 'images/user/perangkat' . $i + 1 . '.jpg',
-        //     ]);
-        // }
-
         PerangkatGampong::create([
             'nik' => '1106152105990002',
             'nama' => 'H. Sabri',
@@ -40,12 +22,132 @@ class PerangkatGampongSeeder extends Seeder
 
         PerangkatGampong::create([
             'nik' => '1106152105990002',
-            'nama' => 'Imran',
+            'nama' => 'Mudasir, S.H',
             'jenis_kelamin' => 'L',
-            'jabatan' => 'Ketua Pemuda',
+            'jabatan' => 'Tuha Peut',
             'no_hp' => '082362568088',
             'alamat' => 'Kota Jantho',
             'photo_path' => 'images/user/2.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Tgk. Marzuki',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Imuem Gampong',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/3.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Ichsan',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Sekretaris Gampong',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/4.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Yudiansyah',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kaur Keuangan',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/5.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Nida Soraya, S.E',
+            'jenis_kelamin' => 'P',
+            'jabatan' => 'Kaur Umum & Perencanaan',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/6.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Isfandiar',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kasi Pemertintahan',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/7.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Amarullah Bachtiar',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kasi Pelayanan',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/8.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Imran',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kasi Kesejahteraan',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/9.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Fachrol, S.T',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kadus Bahagia',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/10.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Ahadur Riza',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kadus Sentosa',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/11.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Budi Wilham',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kadus Seroja',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/12.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Abdul Hamid',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kadus Kemuning',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/13.jpg',
+        ]);
+
+        PerangkatGampong::create([
+            'nik' => '1106152105990002',
+            'nama' => 'Mulyana, S.Pd., M.Pd',
+            'jenis_kelamin' => 'L',
+            'jabatan' => 'Kadus Seulanga',
+            'no_hp' => '082362568088',
+            'alamat' => 'Kota Jantho',
+            'photo_path' => 'images/user/14.jpg',
         ]);
     }
 }
